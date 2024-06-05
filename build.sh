e9c351783c88af4233b1f8d6d2483d12aa77d417
Lukas Straub <lukasstraub2@web.de>
2024-06-05 13:28:45 +0200

    Project name is rootlink

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-cc -fPIC -fvisibility=hidden -fno-omit-frame-pointer -shared -O1 -g -Wall -Wno-unused-function -Wno-comment -Wno-nonnull-compare -Wmissing-prototypes -o main.so \
+cc -fPIC -fvisibility=hidden -fno-omit-frame-pointer -shared -O1 -g -Wall -Wno-unused-function -Wno-comment -Wno-nonnull-compare -Wmissing-prototypes -o rootlink.so \
 	util.c intercept_glibc.c rootshim.c rootlink.c noxattrs.c signalshim.c hardlinkshim.c main.c
