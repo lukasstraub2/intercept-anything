ef0ef4a9509eeda1fe61bf11920042099d45b3c2
Lukas Straub <lukasstraub2@web.de>
2025-10-27 11:36:29 +0200

    Port new clone trampoline assembly to aarch64

--- include/_syscall_trampo.h
+++ include/_syscall_trampo.h
@@ -169,6 +169,18 @@ __asm__(
     "hlt\n"
 );
 #elif defined(__aarch64__)
+__attribute__((unused)) static unsigned long get_savedreg(void* ucontext) {
+    struct ucontext* ctx = (struct ucontext*)ucontext;
+
+    return ctx->uc_mcontext.regs[6];
+}
+
+__attribute__((unused)) static void set_savedreg(void* ucontext, unsigned long reg) {
+    struct ucontext* ctx = (struct ucontext*)ucontext;
+
+    ctx->uc_mcontext.regs[6] = reg;
+}
+
 // there is no absolute jump on aarch64 and syscalls
 // do not clobber any registers.
 // hacky, but clobber x8 (syscall number) or x16 (ip0) or x17 (ip1)
@@ -176,10 +188,120 @@ __asm__(
 __asm__(
 "syscall_trampo_start:\n\t"
     "svc 0\n\t"
-    "ldr x8, syscall_trampo_addr\n\t"
+    "ldr x1, [x6, data.arg2]\n\t"
+    "ldr x2, [x6, data.arg3]\n\t"
+    "ldr x3, [x6, data.arg4]\n\t"
+    "ldr x4, [x6, data.arg5]\n\t"
+    "ldr x5, [x6, data.arg6]\n\t"
+    "mov x1, x6\n\t"
+    "ldr x6, [x1, data.savedreg]\n\t"
+    "ldr x8, [x1, data.jmp_addr]\n\t"
+    "str xzr, [x1, data.refcnt1]\n\t"
     "br x8\n\t"
     "wfi\n"
 );
+
+__asm__(
+"clone_trampo_start:\n\t"
+    "svc 0\n\t"
+    "mov x5, x0\n\t"
+    "cmp x0, #0\n\t"
+    "b.eq child\n\t"
+    "ldr x1, [x6, data.vfork_idx_addr]\n\t"
+    "cmp x1, #0\n\t"
+    "b.eq 1f\n\t"
+    "ldr x2, [x1]\n\t"
+    "sub x2, x2, #1\n\t"
+    "str x2, [x1]\n\t"
+"1:\n\t"
+    "cmp x0, #0\n\t"
+    "b.gt skip_sig_sys\n\t"
+    "ldr x1, [x6, data.set_tid_addr]\n\t"
+    "cmp x1, #0\n\t"
+    "b.eq 1f\n\t"
+    "str xzr, [x1]\n"
+"1:\n\t"
+    "str xzr, [x6, data.refcnt2]\n\t"
+    "b skip_sig_sys\n"
+"child:\n\t"
+    "ldr x1, [x6, data.set_tid_addr]\n\t"
+    "cmp x1, #0\n\t"
+    "b.eq 1f\n\t"
+    "mov x8, #" str(__NR_gettid) "\n\t"
+    "svc 0\n\t"
+    "str x0, [x1]\n"
+"1:\n\t"
+    "ldr x1, [x6, data.sig_dfl_addr]\n\t"
+    "cmp x1, #0\n\t"
+    "b.eq skip_sig_dfl\n\t"
+    "mov x4, #64\n\t"
+    "mov x2, #0\n\t"
+    "mov x3, #8\n"
+"sig_dfl_loop:\n\t"
+    "mov x8, #" str(__NR_rt_sigaction) "\n\t"
+    "mov x0, x4\n\t"
+    "svc 0\n\t"
+    "cmp x0, #0\n\t"
+    "b.ne abort\n"
+"0:\n\t"
+    "add x1, x1, #32\n\t"
+    "sub x4, x4, #1\n\t"
+    "cmp x4, #" str(SIGKILL) "\n\t"
+    "b.eq 0b\n\t"
+    "cmp x4, #" str(SIGSTOP) "\n\t"
+    "b.eq 0b\n\t"
+    "cmp x4, #0\n\t"
+    "b.ne sig_dfl_loop\n"
+"skip_sig_dfl:\n\t"
+    "ldr x1, [x6, data.sig_sys_addr]\n\t"
+    "cmp x1, #0\n\t"
+    "b.eq skip_sig_sys\n\t"
+    "mov x8, #" str(__NR_rt_sigaction) "\n\t"
+    "mov x0, #" str(SIGSYS) "\n\t"
+    "mov x2, #0\n\t"
+    "mov x3, #8\n\t"
+    "svc 0\n\t"
+    "cmp x0, #0\n\t"
+    "b.ne abort\n"
+"skip_sig_sys:\n\t"
+    "ldr x1, [x6, data.sig_mask]\n\t"
+    "cmp x1, #0\n\t"
+    "b.eq skip_sig_mask\n\t"
+    "mov x8, #" str(__NR_rt_sigprocmask) "\n\t"
+    "mov x0, #" str(SIG_SETMASK) "\n\t"
+    "mov x2, #0\n\t"
+    "mov x3, #8\n\t"
+    "svc 0\n\t"
+    "cmp x0, #0\n\t"
+    "b.ne abort\n"
+"skip_sig_mask:\n\t"
+    "mov x0, x5\n\t"
+    "ldr x2, [x6, data.arg3]\n\t"
+    "ldr x3, [x6, data.arg4]\n\t"
+    "ldr x4, [x6, data.arg5]\n\t"
+    "ldr x5, [x6, data.arg6]\n\t"
+    "mov x1, x6\n\t"
+    "ldr x6, [x1, data.savedreg]\n\t"
+    "ldr x8, [x1, data.jmp_addr]\n\t"
+    "cmp x0, #0\n\t"
+    "b.eq 1f\n\t"
+    "str xzr, [x1, data.refcnt1]\n\t"
+    "br x8\n\t"
+    "wfi\n"
+"1:\n\t"
+    "str xzr, [x1, data.refcnt2]\n\t"
+    "br x8\n\t"
+    "wfi\n"
+"abort:\n\t"
+    "mov x8, #" str(__NR_gettid) "\n\t"
+    "svc 0\n\t"
+    "mov x0, x1\n\t"
+    "mov x1, #" str(SIGSYS) "\n\t"
+    "mov x8, #" str(__NR_tkill) "\n\t"
+    "svc 0\n\t"
+    "wfi\n"
+);
+
 #else
 #error Unsupported Architecture
 #endif
