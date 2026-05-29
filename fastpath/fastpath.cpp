cf2555d05e0ebd93c97dac920959c7585ae2b308
Lukas Straub <lukasstraub2@web.de>
2026-05-29 19:34:58 +0000

    Add fastpath module

---
+++ fastpath/fastpath.cpp
@@ -0,0 +1,49 @@
+
+#include "fastpath.h"
+#include "emulate_file_tmp.h"
+#include "fastpath_preload.h"
+#include "fastpath_preload_data.h"
+#include "intercept.h"
+
+class FastPath : public EmulateFileTmp {
+    public:
+    FastPath(CallHandler* next) : EmulateFileTmp(next) {}
+
+    protected:
+    FileAction* _mangle_path(int dirfd, const char* path) override;
+};
+
+FileAction* FastPath::_mangle_path(int dirfd, const char* path) {
+    if (!strcmp(path, "/etc/ld.so.preload") ||
+        !strcmp(path, "/etc/ld-nix.so.preload")) {
+        const char* content = "/intercept-anything/preload.so\n";
+        size_t content_len = strlen(content);
+        FileAction* action =
+            (FileAction*)malloc(sizeof(FileAction) + content_len);
+        *action = {0, 0400, content_len};
+        memcpy(action->data, content, content_len);
+        return action;
+    } else if (!strcmp(path, "/intercept-anything/preload.so")) {
+        const unsigned char* content = FASTPATH_PRELOAD_DATA;
+        size_t content_len = FASTPATH_PRELOAD_DATA_SIZE;
+        FileAction* action =
+            (FileAction*)malloc(sizeof(FileAction) + content_len);
+        *action = {0, 0500, content_len};
+        memcpy(action->data, content, content_len);
+        return action;
+    } else if (!strcmp(path, PRELOAD_ENTRY_FILE)) {
+        fastpath_entry_t content = fastpath_entry;
+        size_t content_len = sizeof(content);
+        FileAction* action =
+            (FileAction*)malloc(sizeof(FileAction) + content_len);
+        *action = {0, 0400, content_len};
+        memcpy(action->data, &content, content_len);
+        return action;
+    }
+
+    return nullptr;
+}
+
+CallHandler* fastpath_init(CallHandler* next) {
+    return new FastPath(next);
+}
---
+++ include/fastpath.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "callhandler.h"
+
+CallHandler* fastpath_init(CallHandler* next);
