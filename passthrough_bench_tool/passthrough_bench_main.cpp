f953a28d178e34167230ac8d60a95cba7ff45b52
Lukas Straub <lukasstraub2@web.de>
2026-05-29 20:15:43 +0000

    Use fastpath in syscall log and passtrough bench

--- passthrough_bench_tool/passthrough_bench_main.cpp
+++ passthrough_bench_tool/passthrough_bench_main.cpp
@@ -1,8 +1,10 @@
 
 #include "workarounds.h"
 #include "signalmanager.h"
+#include "fastpath.h"
 
 CallHandler* main_init(CallHandler* const bottom, int recursing) {
     signalmanager_skip_enable_signals(1);
-    return workarounds_init(bottom);
+    CallHandler* fastpath = fastpath_init(bottom);
+    return workarounds_init(fastpath);
 }
--- syscall_log_tool/syscall_log_main.cpp
+++ syscall_log_tool/syscall_log_main.cpp
@@ -1,9 +1,11 @@
 
+#include "fastpath.h"
 #include "workarounds.h"
 #include "syscall_log.h"
 
 CallHandler* main_init(CallHandler* const bottom, int recursing) {
-    CallHandler* workarounds = workarounds_init(bottom);
+    CallHandler* fastpath = fastpath_init(bottom);
+    CallHandler* workarounds = workarounds_init(fastpath);
     CallHandler* logger = syscall_log_init(workarounds);
     return logger;
 }
