b648b3364951cfc22ed4fb574dd52dd1757a5c66
Lukas Straub <lukasstraub2@web.de>
2026-05-30 22:25:53 +0000

    Add more fastpaths

--- fastpath/fastpath_preload.c
+++ fastpath/fastpath_preload.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/uio.h>
+#include <sys/socket.h>
 
 #ifdef _FILE_OFFSET_BITS
 #undef _FILE_OFFSET_BITS
@@ -44,9 +45,7 @@ static void maybe_init() {
 }
 
 #undef read
-__attribute__((visibility("default"))) ssize_t read(int fd,
-                                                    void* data,
-                                                    size_t len) {
+ssize_t read(int fd, void* data, size_t len) {
     ssize_t ret;
 
     maybe_init();
@@ -61,9 +60,7 @@ __attribute__((visibility("default"))) ssize_t read(int fd,
 }
 
 #undef readv
-__attribute__((visibility("default"))) ssize_t readv(int fd,
-                                                     const struct iovec* iov,
-                                                     int iovcnt) {
+ssize_t readv(int fd, const struct iovec* iov, int iovcnt) {
     ssize_t ret;
 
     maybe_init();
@@ -78,10 +75,7 @@ __attribute__((visibility("default"))) ssize_t readv(int fd,
 }
 
 #undef preadv
-__attribute__((visibility("default"))) ssize_t preadv(int fd,
-                                                      const struct iovec* iov,
-                                                      int iovcnt,
-                                                      off_t off) {
+ssize_t preadv(int fd, const struct iovec* iov, int iovcnt, off_t off) {
     ssize_t ret;
 
     maybe_init();
@@ -97,8 +91,11 @@ __attribute__((visibility("default"))) ssize_t preadv(int fd,
 }
 
 #undef preadv2
-__attribute__((visibility("default"))) ssize_t
-preadv2(int fd, const struct iovec* iov, int iovcnt, off_t off, int flags) {
+ssize_t preadv2(int fd,
+                const struct iovec* iov,
+                int iovcnt,
+                off_t off,
+                int flags) {
     ssize_t ret;
 
     maybe_init();
@@ -114,9 +111,7 @@ preadv2(int fd, const struct iovec* iov, int iovcnt, off_t off, int flags) {
 }
 
 #undef write
-__attribute__((visibility("default"))) ssize_t write(int fd,
-                                                     const void* data,
-                                                     size_t len) {
+ssize_t write(int fd, const void* data, size_t len) {
     ssize_t ret;
 
     maybe_init();
@@ -131,9 +126,7 @@ __attribute__((visibility("default"))) ssize_t write(int fd,
 }
 
 #undef writev
-__attribute__((visibility("default"))) ssize_t writev(int fd,
-                                                      const struct iovec* iov,
-                                                      int iovcnt) {
+ssize_t writev(int fd, const struct iovec* iov, int iovcnt) {
     ssize_t ret;
 
     maybe_init();
@@ -148,10 +141,7 @@ __attribute__((visibility("default"))) ssize_t writev(int fd,
 }
 
 #undef pwritev
-__attribute__((visibility("default"))) ssize_t pwritev(int fd,
-                                                       const struct iovec* iov,
-                                                       int iovcnt,
-                                                       off_t off) {
+ssize_t pwritev(int fd, const struct iovec* iov, int iovcnt, off_t off) {
     ssize_t ret;
 
     maybe_init();
@@ -167,14 +157,149 @@ __attribute__((visibility("default"))) ssize_t pwritev(int fd,
 }
 
 #undef pwritev2
-__attribute__((visibility("default"))) ssize_t
-pwritev2(int fd, const struct iovec* iov, int iovcnt, off_t off, int flags) {
+ssize_t pwritev2(int fd,
+                 const struct iovec* iov,
+                 int iovcnt,
+                 off_t off,
+                 int flags) {
     ssize_t ret;
 
     maybe_init();
 
     ret = entry(__NR_pwritev2, fd, (unsigned long)iov, iovcnt, (long)(off),
                 (long)(off >> 32), flags);
+    if (ret < 0) {
+        errno = -ret;
+        return -1;
+    }
+
+    return ret;
+}
+
+#undef sendto
+ssize_t sendto(int fd,
+               const void* buf,
+               size_t len,
+               int flags,
+               const struct sockaddr* addr,
+               socklen_t addr_len) {
+    ssize_t ret;
+
+    maybe_init();
+
+    ret = entry(__NR_sendto, fd, (unsigned long)buf, len, flags,
+                (unsigned long)addr, addr_len);
+    if (ret < 0) {
+        errno = -ret;
+        return -1;
+    }
+
+    return ret;
+}
+
+#undef send
+ssize_t send(int fd, const void* buf, size_t len, int flags) {
+    return sendto(fd, buf, len, flags, NULL, 0);
+}
+
+#undef sendmsg
+ssize_t sendmsg(int fd, const struct msghdr* msg, int flags) {
+    ssize_t ret;
+
+    maybe_init();
+
+    ret = entry(__NR_sendmsg, fd, (unsigned long)msg, flags, 0, 0, 0);
+    if (ret < 0) {
+        errno = -ret;
+        return -1;
+    }
+
+    return ret;
+}
+
+#undef sendmmsg
+int sendmmsg(int fd, struct mmsghdr* msgvec, unsigned int vlen, int flags) {
+    int ret;
+
+    maybe_init();
+
+    ret = entry(__NR_sendmmsg, fd, (unsigned long)msgvec, vlen, flags, 0, 0);
+    if (ret < 0) {
+        errno = -ret;
+        return -1;
+    }
+
+    return ret;
+}
+
+#undef recvfrom
+ssize_t recvfrom(int fd,
+                 void* buf,
+                 size_t len,
+                 int flags,
+                 struct sockaddr* addr,
+                 socklen_t* addr_len) {
+    ssize_t ret;
+
+    maybe_init();
+
+    ret = entry(__NR_recvfrom, fd, (unsigned long)buf, len, flags,
+                (unsigned long)addr, (unsigned long)addr_len);
+    if (ret < 0) {
+        errno = -ret;
+        return -1;
+    }
+
+    return ret;
+}
+
+#undef recv
+ssize_t recv(int fd, void* buf, size_t len, int flags) {
+    return recvfrom(fd, buf, len, flags, NULL, NULL);
+}
+
+#undef recvmsg
+ssize_t recvmsg(int fd, struct msghdr* msg, int flags) {
+    ssize_t ret;
+
+    maybe_init();
+
+    ret = entry(__NR_recvmsg, fd, (unsigned long)msg, flags, 0, 0, 0);
+    if (ret < 0) {
+        errno = -ret;
+        return -1;
+    }
+
+    return ret;
+}
+
+#undef recvmmsg
+int recvmmsg(int fd,
+             struct mmsghdr* msgvec,
+             unsigned int vlen,
+             int flags,
+             struct timespec* timeout) {
+    int ret;
+
+    maybe_init();
+
+    ret = entry(__NR_recvmmsg, fd, (unsigned long)msgvec, vlen, flags,
+                (unsigned long)timeout, 0);
+    if (ret < 0) {
+        errno = -ret;
+        return -1;
+    }
+
+    return ret;
+}
+
+#undef msync
+int msync(void* addr, size_t len, int flags) {
+    int ret;
+
+    maybe_init();
+
+    ret = entry(__NR_msync, (unsigned long)addr, len, flags, 0, 0, 0);
     if (ret < 0) {
         errno = -ret;
         return -1;
